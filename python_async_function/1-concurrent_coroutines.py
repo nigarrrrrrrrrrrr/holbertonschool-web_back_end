2b27b98d13ee46e52e84c663da167943eeeae0c2
nigar <nigarsuleymanova73@gmail.com>
2026-04-12 18:38:02 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-""" Let's execute multiple coroutines at the same time with async """
+"""
+Bu modul eyni vaxtda icra olunan asinxron coroutine-ləri idarə edir.
+Burada wait_n funksiyası vasitəsilə çox sayda tapşırıq işə salınır.
+"""
 import asyncio
 from typing import List
-import random
 
-
+# wait_random funksiyasını verilən tapşırığa uyğun idxal edirik
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Function that returns the list
     """
-    l = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
-    finish = [await task for task in asyncio.as_completed(l)]
-    return finish
+    n dəfə wait_random coroutine-ni çağırır və nəticələri
+    hazır olma ardıcıllığına (artan sıra) uyğun olaraq qaytarır.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    
+    # as_completed(tasks) siyahıdakı tapşırıqları bitdikcə qaytarır
+    delays = [await task for task in asyncio.as_completed(tasks)]
+    
+    return delays
+    
