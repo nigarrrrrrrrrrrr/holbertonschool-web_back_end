f039bcc8260c63861e196bb112ddf84ac951c95e
nigar <nigarsuleymanova73@gmail.com>
2026-04-12 19:10:25 +0400

    Update wait_n to collect delays using callbacks
    Refactor wait_n to use callbacks for collecting delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-"""
-Bu modul eyni vaxtda icra olunan asinxron coroutine-ləri idarə edir.
-Burada wait_n funksiyası vasitəsilə çox sayda tapşırıq işə salınır.
-"""
-import asyncio
-from typing import List
+''' Description: Import wait_random from the previous python file that
+                 you’ve written and write an async routine called wait_n
+                 that takes in 2 int arguments: max_delay and n. You will
+                 spawn wait_random n times with the specified max_delay.
 
-# wait_random funksiyasını verilən tapşırığa uyğun idxal edirik
+                 wait_n should return the list of all the delays(float values)
+                 The list of the delays should be in ascending order without
+                 using sort() because of concurrency.
+    Arguments: n: int, max_delay: int = 10
+'''
+
+import asyncio
+import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    n dəfə wait_random coroutine-ni çağırır və nəticələri
-    hazır olma ardıcıllığına (artan sıra) uyğun olaraq qaytarır.
-    """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    
-    # as_completed(tasks) siyahıdakı tapşırıqları bitdikcə qaytarır
-    delays = [await task for task in asyncio.as_completed(tasks)]
-    
-    return delays
-    
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
