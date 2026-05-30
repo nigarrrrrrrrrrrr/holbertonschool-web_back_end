3fbdbf5c251e50ca6ad5290f9112b3a8392b1867
nigar <nigarsuleymanova73@gmail.com>
2026-05-30 23:17:43 +0400

    Refactor Babel configuration and update routes
    Refactor Flask app configuration and improve comments.

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
-''' Flask app '''
-
-from flask import Flask, request, render_template
+""" Basic Babel setup """
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config:
-    ''' App config '''
+class Config(object):
+    """ Configuration Babel """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    ''' return best languages '''
+    """ Locale language
+
+        Return:
+            Best match to the language
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_world():
-    ''' return the template '''
-    return render_template('2-index.html')
+    """ Greeting
+
+        Return:
+            Initial template html
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == '__main__':
-    app.run()rt="5000")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
