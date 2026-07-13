f56da883db1601cb32551f99b52f4418e7b7d7d2
nigar <nigarsuleymanova73@gmail.com>
2026-07-13 19:29:04 +0400

    Refactor Flask app configuration and routes

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,40 +1,34 @@
 #!/usr/bin/env python3
-""" Basic Babel setup """
+"""Flask app that determines locale from Accept-Language header."""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config(object):
-    """ Configuration Babel """
+class Config:
+    """Configuration class for the Flask application."""
+
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """ Locale language
-
-        Return:
-            Best match to the language
-    """
+def get_locale() -> str:
+    """Determine best locale from request Accept-Language header."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """ Greeting
-
-        Return:
-            Initial template html
-    """
-    return render_template('1-index.html')
+babel = Babel(app, locale_selector=get_locale)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+@app.route('/')
+def index() -> str:
+    """Render the index page."""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
