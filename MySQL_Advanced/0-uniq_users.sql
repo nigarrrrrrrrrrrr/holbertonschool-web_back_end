07bc5399ffa8c23f26ccc17c3979719ed2ddd0b0
nigar <nigarsuleymanova73@gmail.com>
2026-07-13 18:11:48 +0400

    Refactor users table creation SQL script
    Updated the SQL script to improve formatting and clarity.

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- Task : 0. We are all unique! - creates a table users
--- script can be executed on any database
-CREATE TABLE If NOT EXISTS `users` (  
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+-- Creates a table users with unique email
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
